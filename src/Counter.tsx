02f8df516ada8c2451a83354400b2df9c35cc3c2
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-24 22:47:45 +0400

    upd: exam create counter component

---
+++ src/Counter.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { Button } from './Button';
+import c from './Counter.module.css'
+
+export const Counter = () => {
+
+	const minValue = 0;
+	const maxValue = 5;
+
+	const [count, setCount] = useState<number>(minValue);
+
+	const increaseCount = () => setCount(count => count + 1);
+	const resetCount = () => setCount(minValue);
+
+	const incDisabled = count === maxValue;
+	const resetDisabled = count === minValue;
+
+	const displayStyle = `${c.counterDisplay} ${incDisabled ? c.red : ''}`
+
+	return (
+		<div className={c.counterBody}>
+			<div className={displayStyle}>{count}</div>
+			<div className={c.counterButtons}>
+				<Button
+					disabled={incDisabled}
+					callback={increaseCount}>inc</Button>
+				<Button
+					disabled={resetDisabled}
+					callback={resetCount}>reset</Button>
+			</div>
+		</div>
+	);
+}
