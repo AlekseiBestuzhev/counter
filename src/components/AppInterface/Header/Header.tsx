93e7f60df02cd3dad7afccd07059ea071f5c4d50
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-03-13 02:40:31 +0400

    fix: header title

--- src/components/AppInterface/Header/Header.tsx
+++ src/components/AppInterface/Header/Header.tsx
@@ -1,12 +1,26 @@
 import c from 'components/AppInterface/Header/Header.module.css'
 import menuIcon from 'menu.svg'
 import { FC } from 'react'
+import { useLocation } from 'react-router-dom'
+import { PATH } from '../Pages/Pages'
 
 type HeaderType = {
 	setMenuOpen: () => void
 }
 
 export const Header: FC<HeaderType> = ({ setMenuOpen }) => {
+
+	const location = useLocation();
+	const currentPath = location.pathname;
+
+	const pageTitle =
+		currentPath === PATH.BASIC_COUNTER
+			? 'Basic Counter'
+			: currentPath === PATH.SETABLE_COUNTER_V1
+				? 'Setable Counter v1'
+				: currentPath === PATH.SETABLE_COUNTER_V2
+					? 'Setable Counter v2'
+					: 'Error'
 
 	return (
 		<div className={c.headerBody}>
@@ -16,7 +30,7 @@ export const Header: FC<HeaderType> = ({ setMenuOpen }) => {
 				onClick={setMenuOpen}
 				className={c.burgerMenuOpen}
 			/>
-			<h1 className={c.title}>Title</h1>
+			<h1 className={c.title}>{pageTitle}</h1>
 		</div>
 	);
 }
